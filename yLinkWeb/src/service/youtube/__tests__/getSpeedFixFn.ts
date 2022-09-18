ec5296de8ed4c98e153c4ada7d2f00a0345038ee
Anton <leonardspbox@gmail.com>
2022-09-18 10:42:55 +0500

    fix yt

--- yLinkWeb/src/service/youtube/__tests__/getSpeedFixFn.ts
+++ yLinkWeb/src/service/youtube/__tests__/getSpeedFixFn.ts
@@ -7,6 +7,22 @@ const fetch = require('node-fetch');
 global.fetch = fetch;
 
 jest.setTimeout(5 * 60 * 1000);
+test('getCurrentPlayer', async () => {
+  const {playerUrl} = await getClientInfo();
+
+  console.log('playerUrl', playerUrl.toString());
+
+  const body = await fetch(playerUrl).then((r: any) => r.text());
+
+  const path = Path.join(__dirname, "./p/base.js");
+
+  await Fs.promises.mkdir(Path.dirname(path), {recursive: true});
+
+  await Fs.promises.writeFile(path, body);
+
+  console.log('done');
+});
+
 test('getSigFn', async () => {
   // const playerUrl = 'https://www.youtube.com/s/player/9216d1f7/player_ias.vflset/en_US/base.js';
 
--- yLinkWeb/src/service/youtube/getSpeedFixFn.ts
+++ yLinkWeb/src/service/youtube/getSpeedFixFn.ts
@@ -18,27 +18,68 @@ async function getSpeedFixFn(playerUrl: string) {
   }
 }
 
+const codeData = function () {
+  const __result__ = {};
+  const navigator = {};
+  const location = {hostname: 'a'};
+  const document = {location, domain: 'a'};
+  /* @ts-ignore */
+  Object.assign(this, {document, location, navigator});
+  const XMLHttpRequest = class {
+    fetch(...args: any[]) {
+
+    };
+  };
+};
+
+const afterCodeData = function () {
+  try {
+    const a = new Map();
+    Object.assign(a, {
+      u: 'local',
+      D: 'true',
+    });
+    a.set('n', 'true');
+    /* @ts-ignore */
+    _yt_player.YB.prototype.get.call(a);
+  }catch(_e){}
+  /* @ts-ignore */
+  if (Array.isArray(__result__.sigFn)) {
+    /* @ts-ignore */
+    __result__.sigFn = __result__.sigFn[0];
+  }
+};
+
+const getFnBody = (fn: () => void) => {
+  const str = fn.toString();
+  const start = str.indexOf('{') + 1;
+  const end = str.lastIndexOf('}');
+  return str.slice(start, end);
+};
+
 export function getSigFn(code: string) {
-  const m = /\([$\w]+=([$\w]+)\([$\w]+\),[$\w]+\.set\("n",/.exec(code);
+  const m = /\.get\("n"\)\)&&\(b=([a-zA-Z0-9$]+)(?:\[\d+\])?\([a-zA-Z0-9]\)/.exec(code);
   if (!m) {
     throw new Error('Sig fn name not found');
   }
-  const name = m[1];
+
+  let [fragment, variable] = m;
+
+  const newFragment = fragment.replace(variable, `(__result__.sigFn=${variable})`);
 
   code = code
-    .replace('var window=this;', 'var window=self;')
-    .replace(new RegExp(`([;\\n]${name})(=function)`, ''), "$1=__result__.sigFn$2");
+    .replace(fragment, newFragment);
 
-  const fn = new Function('', `
-  const __result__ = {};
-  const navigator = {};
-  const location = {hostname:'a'};
-  const document = {location,domain:'a'};
-  const window = {document,location};
-  const self = window;
-  ${code};
-  return __result__.sigFn;
-`)();
+  // console.log({newFragment});
+
+  const pre = getFnBody(codeData);
+
+  // console.log(pre);
+
+  const fn = new Function('', `${pre.replace(/\n/g, ' ')}
+${code};
+${getFnBody(afterCodeData)}
+return __result__.sigFn;`)();
 
   if (typeof fn !== "function") {
     throw new Error('Sig fn is not found');
